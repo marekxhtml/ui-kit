c5bb7d71a3b0e543acd7d3336f801f7990057430
ylakhdar <ylakhdar@coveo.com>
2025-02-24 15:14:47 -0500

    apply corrections
    https://coveord.atlassian.net/browse/KIT-3948

--- packages/atomic/src/components/commerce/lazy-index.ts
+++ packages/atomic/src/components/commerce/lazy-index.ts
@@ -1,6 +1,6 @@
 export default {
   // Add entries as such when new components are added/moved to Lit.
-  // 'atomic-commerce-breadbox': async () => await import('./atomic-commerce-breadbox/atomic-commerce-breadbox.js'),
+  'atomic-icon': async () => await import('../common/atomic-icon/atomic-icon'),
 } as Record<string, () => Promise<unknown>>;
 
 export type * from './index.js';
